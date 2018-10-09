e12f2ec86383eeb8a6f00d3ad5c456d41ccb2406
andruha81 <43496275+andruha81@users.noreply.github.com>
2018-10-10 00:14:36 +0300

    Homework four

--- src/khaustau/homeworkFour/space/Main.java
+++ src/khaustau/homeworkFour/space/Main.java
@@ -1,24 +1,84 @@
 package khaustau.homeworkFour.space;
 
-import khaustau.homeworkFour.space.entity.Ariane;
-import khaustau.homeworkFour.space.entity.Falcon;
-import khaustau.homeworkFour.space.entity.LaunchVechicle;
+import khaustau.homeworkFour.space.entity.MeteoSat;
+import khaustau.homeworkFour.space.entity.NavSat;
+import khaustau.homeworkFour.space.entity.Satallite;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-        LaunchVechicle falcon;
-        LaunchVechicle ariane;
+        List<Satallite> succes = new ArrayList();
+        List<Satallite> fault = new ArrayList();
+        Random rand = new Random();
 
-        ariane = new Ariane();
-        ariane.production();
-        ariane.launching();
-        ariane.landing();
+        LOOP : for (int i = 1; i <= 10; i++) {
+            switch (rand.nextInt(2) + 1) {
+                case 1 :
+                {
+                    MeteoSat meteo = new MeteoSat("BelWeatherSat", 12, 2.6);
+                    meteo.setName(meteo.counter);
+                    while (true) {
+                        if (!meteo.launch("Ariane 5")) {
+                            meteo.crash();
+                            fault.add(meteo);
+                            continue LOOP;
+                        }
+                        if (!meteo.reachDestination()) {
+                            meteo.crash();
+                            fault.add(meteo);
+                            continue LOOP;
+                        }
+                        meteo.operate();
+                        meteo.utilization();
+                        System.out.println();
+                        succes.add(meteo);
+                        continue LOOP;
+                    }
+                }
+                case 2 :
+                {
+                    NavSat navsat = new NavSat("Galileo", 15, 1.58);
+                    navsat.setName(navsat.counter);
+                    while (true) {
+                        if (!navsat.launch("Souz 2")) {
+                            navsat.crash();
+                            fault.add(navsat);
+                            continue LOOP;
+                        }
+                        if (!navsat.reachDestination()) {
+                            navsat.crash();
+                            fault.add(navsat);
+                            continue LOOP;
+                        }
+                        navsat.operate();
+                        navsat.utilization();
+                        System.out.println();
+                        succes.add(navsat);
+                        continue LOOP;
+                    }
+                }
+            }
+        }
 
-        falcon = new Falcon();
-        falcon.production();
-        falcon.testing();
-        falcon.launching();
-        falcon.landing();
+        if (fault.isEmpty() == false) {
+            System.out.println("Unsuccessful satellites : ");
+            for (Satallite sat : fault) {
+                System.out.println(sat.getName() + ", " + sat.getType() + ", mass " +
+                        sat.getMass() + ", life time " + sat.getLifeTime());
+            }
+            System.out.println();
+        }
+
+        if (succes.isEmpty() == false) {
+            System.out.println("Successful satellites : ");
+            for (Satallite sat : succes) {
+                System.out.println(sat.getName() + ", " + sat.getType() + ", mass " +
+                        sat.getMass() + ", life time " + sat.getLifeTime());
+            }
+        }
     }
 }
